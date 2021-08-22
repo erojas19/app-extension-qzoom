3581503821752e4ef2e7c8b304b80141f1999419
erojas19 <efrainrojas7@gmail.com>
2021-08-22 15:14:09 -0400

    Qzoom for vue 3

--- src/boot/qzoom.js
+++ src/boot/qzoom.js
@@ -1,5 +1,8 @@
 import QZoom from '@quasar/quasar-app-extension-qzoom/src/component/QZoom'
 
-export default ({ Vue }) => {
-  Vue.component('QZoom', QZoom)
+export default {
+    install: (app, options) => {
+        /* declare global component */      
+        app.component('QZoom', QZoom)
+    }
 }
